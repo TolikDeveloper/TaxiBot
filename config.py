61a926aba582b05942bbcf1e62eb58957d09f09d
TolikDeveloper <83750170+TolikDeveloper@users.noreply.github.com>
2025-11-04 13:39:21 +0500

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-
-BOT_TOKEN = "8509764843:AAEOqn1Kaf8-n0OZXBizcGCLz_-OuYo7cO0"
-ADMIN_CHAT_ID = "-1003139491276"
-
-# Agar Render.com da ishlatayotgan bo‘lsang, domenni quyidagicha yoz:
-WEBHOOK_URL = "https://taxibot-1-6gwt.onrender.com"
